52faeeab3234c78fa1832aac37ad39c6605ec3aa
Lunden <77023182+Plekzz@users.noreply.github.com>
2024-02-12 09:03:59 +0100

    PV2

---
+++ script.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+
+
+function DVHov() {
+	var image = document.getElementById("DV")
+	image.style.opacity = "1"
+	
+	var image = document.getElementById("LPV")
+	image.style.opacity = "0"
+}
+
+function PVHov() {
+var image = document.getElementById("LPV")
+	image.style.opacity = "1"
+	
+var image = document.getElementById("DV")
+	image.style.opacity = "0"
+
+}
+
